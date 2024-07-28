cd4373f52f42b56bbfc5499ddac585720fd86eec
IshikaT21 <ishikatiwari416@gmail.com>
2024-07-28 17:44:21 +0530

    Updated routes

--- server/routes.js
+++ server/routes.js
@@ -11,13 +11,13 @@ const { addTags, deleteTags, query } = require("./Controllers/tags");
 
 const router = express.Router();
 
-router.post("/createNote", createNote);
-router.get("/getAllNotes", getAllNotes);
-router.delete("/deleteNote/:id", deleteNoteById);
-router.put("/updateNote/:id", updateNoteById);
-router.get("/getNoteById/:id", getNoteById);
-router.put("/addTag/:id", addTags);
-router.delete("/deleteTag/:id", deleteTags);
+router.post("/notes", createNote);
+router.get("/notes", getAllNotes);
+router.delete("/notes/:id", deleteNoteById);
+router.put("/notes/:id", updateNoteById);
+router.get("/notes/:id", getNoteById);
+router.put("/notes/:id/tags", addTags);
+router.delete("/notes/:id/tags", deleteTags);
 router.get("/query", query);
 
 module.exports = router;
